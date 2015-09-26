214640b6a30add0415969f347f71c017a8f045d7
lapoiz <poizatd@hotmail.com>
2015-09-26 23:12:26 +0200

    LaPoizGraph add

--- src/LaPoiz/GraphBundle/Resources/public/js/Graph.js
+++ src/LaPoiz/GraphBundle/Resources/public/js/Graph.js
@@ -1,4 +1,5 @@
 const SvgNS='http://www.w3.org/2000/svg';
+const SvgXlinkns='http://www.w3.org/1999/xlink';
 
 /**
  *
@@ -15,9 +16,29 @@ isEmpty = function(variable) {
     return variable === undefined || variable === null || variable === '' || variable.length === 0;
 }
 
+// X et Y selon les normes classiques du svg, cad O : en haut à gauche
+
+Graph.prototype.newPolygonSVGElement = function(points,className,svgElem) {
+    var newPolygon=document.createElementNS(SvgNS,'polygon');
+    if (!isEmpty(points)) {
+        newPolygon.setAttributeNS(null,'points',points);
+    }
+    if (!isEmpty(className)) {
+        newPolygon.setAttributeNS(null,'class',className);
+    }
+    if (isEmpty(svgElem)) {
+        this.svg.appendChild(newPolygon);
+    } else {
+        svgElem.appendChild(newPolygon);
+    }
+
+    return newPolygon;
+}
 Graph.prototype.newPathSVGElement = function(path,className,svgElem) {
     var newPath=document.createElementNS(SvgNS,'path');
-    newPath.setAttributeNS(null,'d',path);
+    if (!isEmpty(path)) {
+        newPath.setAttributeNS(null,'d',path);
+    }
     if (!isEmpty(className)) {
         newPath.setAttributeNS(null,'class',className);
     }
@@ -52,11 +73,33 @@ Graph.prototype.newRectSVGElement = function(x,y,width,height,className,idName,s
     }
     return newRect;
 }
-Graph.prototype.newTextSVGElement = function(x,y,text,className,svgElem) {
+Graph.prototype.newCircleSVGElement = function(x,y,rayon,className,idName,svgElem) {
+    var newCircle=document.createElementNS(SvgNS,'circle');
+    newCircle.setAttributeNS(null,'cx',x);
+    newCircle.setAttributeNS(null,'cy',y);
+    if (!isEmpty(rayon)) {
+        newCircle.setAttributeNS(null,'r',rayon);
+    }
+    if (!isEmpty(className)) {
+        newCircle.setAttributeNS(null,'class',className);
+    }
+    if (!isEmpty(idName)) {
+        newCircle.setAttributeNS(null,'id',idName);
+    }
+    if (isEmpty(svgElem)) {
+        this.svg.appendChild(newCircle);
+    } else {
+        svgElem.appendChild(newCircle);
+    }
+    return newCircle;
+}
+Graph.prototype.newTextSVGElement = function(x,y,text,className,svgElem,idName) {
     var newText=document.createElementNS(SvgNS,'text');
     newText.setAttributeNS(null,'x',x);
     newText.setAttributeNS(null,'y',y);
-    newText.appendChild(document.createTextNode(text));
+    if (!isEmpty(text)) {
+        newText.appendChild(document.createTextNode(text));
+    }
     if (!isEmpty(className)) {
         newText.setAttributeNS(null,'class',className);
     }
@@ -65,12 +108,45 @@ Graph.prototype.newTextSVGElement = function(x,y,text,className,svgElem) {
     } else {
         svgElem.appendChild(newText);
     }
+
+    if (!isEmpty(idName)) {
+        newText.setAttributeNS(null,'id',idName);
+    }
     return newText;
 }
-Graph.prototype.newGroupSVGElement = function(x,y,width,height,className,idName,svgElem) {
+Graph.prototype.newTSpanSVGElement = function(dx,dy,text,className,svgElem,idName) {
+    var newTspan=document.createElementNS(SvgNS,'tspan');
+    if (!isEmpty(dx)) {
+        newTspan.setAttributeNS(null,'dx',dx);
+    }
+    if (!isEmpty(dy)) {
+        newTspan.setAttributeNS(null,'dy',dy);
+    }
+    if (!isEmpty(text)) {
+        newTspan.appendChild(document.createTextNode(text));
+    }
+    if (!isEmpty(className)) {
+        newTspan.setAttributeNS(null,'class',className);
+    }
+    if (isEmpty(svgElem)) {
+        this.svg.appendChild(newTspan);
+    } else {
+        svgElem.appendChild(newTspan);
+    }
+
+    if (!isEmpty(idName)) {
+        newTspan.setAttributeNS(null,'id',idName);
+    }
+    return newTspan;
+}
+Graph.prototype.newGroupSVGElement = function(x,y,idName,width,height,className,svgElem) {
     var newGroup=document.createElementNS(SvgNS,'g');
-    newGroup.setAttributeNS(null,'x',x);
-    newGroup.setAttributeNS(null,'y',y);
+    if (!isEmpty(x)) {
+        newGroup.setAttributeNS(null,'x',x);
+    }
+    if (!isEmpty(y)) {
+        newGroup.setAttributeNS(null,'y',y);
+    }
     if (!isEmpty(width)) {
         newGroup.setAttributeNS(null,'width',width);
     }
@@ -112,7 +188,72 @@ Graph.prototype.addStopLinearGradientSVGElement = function(svgElem, idName, offs
     svgElem.appendChild(newstop);
     return newstop;
 }
+Graph.prototype.newAnimate=function(attributeName,attributeType,begin,dur,from,to,svgElem,fill) {
+    var animate=document.createElementNS(SvgNS,'animate');
+    animate.setAttributeNS(null,'attributeName',attributeName);
+    animate.setAttributeNS(null,'attributeType',attributeType);
+    animate.setAttributeNS(null,'begin',begin);
+    animate.setAttributeNS(null,'dur',dur);
+    animate.setAttributeNS(null,'from',from);
+    animate.setAttributeNS(null,'to',to);
+    if (!isEmpty(fill)) {
+        animate.setAttributeNS(null,'fill',fill);
+    }
 
+    svgElem.appendChild(animate);
+}
+Graph.prototype.newSymbolSVGElement = function(idName,viewBox,preserveAspectRatio) {
+    var newSymbol=document.createElementNS(SvgNS,'symbol');
+    if (!isEmpty(idName)) {
+        newSymbol.setAttributeNS(null,'id',idName);
+    }
+    if (!isEmpty(viewBox)) {
+        newSymbol.setAttributeNS(null,'viewBox',viewBox);
+    } else {
+        newSymbol.setAttributeNS(null,'viewBox','0 0 100 100');
+    }
+
+    if (!isEmpty(preserveAspectRatio)) {
+        newSymbol.setAttributeNS(null,'preserveAspectRatio',preserveAspectRatio);
+    } else {
+        newSymbol.setAttributeNS(null,'preserveAspectRatio','xMidYMid meet');
+    }
+    this.defsSvgGraph.appendChild(newSymbol);
+    return newSymbol;
+}
+Graph.prototype.newUseSVGElement = function(symbolName,x,y,width,heigth,transform,className,svgElem) {
+    var newUse=document.createElementNS(SvgNS,'use');
+    if (!isEmpty(symbolName)) {
+        newUse.setAttributeNS(SvgXlinkns,'xlink:href','#'+symbolName);
+    }
+    if (!isEmpty(x)) {
+        newUse.setAttributeNS(null,'x',x);
+    }
+    if (!isEmpty(y)) {
+        newUse.setAttributeNS(null,'y',y);
+    }
+    if (!isEmpty(width)) {
+        newUse.setAttributeNS(null,'width',width);
+    }
+    if (!isEmpty(heigth)) {
+        newUse.setAttributeNS(null,'heigth',heigth);
+    }
+    if (!isEmpty(transform)) {
+        newUse.setAttributeNS(null,'transform',transform);
+    }
+
+    if (!isEmpty(className)) {
+        newUse.setAttributeNS(null,'class',className);
+    }
+
+    if (!isEmpty(svgElem)) {
+        svgElem.appendChild(newUse);
+    } else {
+        this.svg.appendChild(newUse)
+    }
+
+    return newUse;
+}
 
 
 
